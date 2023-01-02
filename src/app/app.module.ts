7fe5b2a26c8ec28ccc65479713c0d9eae4c2aa32
ErhanTolek <erhantolek2207@gmail.com>
2023-01-02 20:34:43 +0300

    InitialCommit-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './Components/heroes/heroes.component';
+import { AuthComponent } from './Components/auth/auth.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SearchComponent } from './Components/search/search.component';
+import { EditComponent } from './Components/edit/edit.component';
+import { MainComponent } from './Components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    AuthComponent,
+    NavbarComponent,
+    SearchComponent,
+    EditComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
